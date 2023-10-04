5fd06f01abfceb1d6c4f39e452dce322ef4c8c42
DzianCPP <dzian.cpp@gmail.com>
2023-10-04 20:27:20 +0300

    bootstrap easy integration

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -12,13 +12,25 @@ class BaseController extends AbstractController
 
     public function __construct()
     {
-        $this->render_data = [
-            'styles' => [
-                '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">'
-            ],
-            'jss' => [
-                '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"crossorigin="anonymous"></script>'
+        $styles = [
+            [
+                'href' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+                'integrity' => "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
+                'crossorigin'=> "anonymous"
             ]
+        ];
+
+        $jss = [
+            [
+                'src' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+                'integrity' => "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM",
+                'crossorigin' => "anonymous"
+            ]
+        ];
+
+        $this->render_data = [
+            'styles' => $styles,
+            'jss' => $jss
         ];
     }
 
