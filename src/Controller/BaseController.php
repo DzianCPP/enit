a4396b1db09d727b6ad8e02a6b12724e3242ead7
DzianCPP <dzian.cpp@gmail.com>
2023-10-05 20:06:05 +0300

    basic layout

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,42 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BaseController extends AbstractController
+abstract class BaseController extends AbstractController
 {
     protected array $render_data = [];
 
-    public function __construct()
+    public function __construct(private RequestStack $requestStack)
     {
-        $styles = [
-            [
-                'href' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
-                'integrity' => "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
-                'crossorigin'=> "anonymous"
-            ]
-        ];
-
-        $jss = [
-            [
-                'src' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
-                'integrity' => "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM",
-                'crossorigin' => "anonymous"
-            ]
-        ];
-
-        $this->render_data = [
-            'styles' => $styles,
-            'jss' => $jss
-        ];
-    }
-
-    #[Route('/base', name: 'app_base')]
-    public function index(): Response
-    {
-        return $this->render('base/index.html.twig', [
-            'controller_name' => 'BaseController',
+        $this->addItemsToRender([
+            'styles' => $this->getStyles(),
+            'jss' => $this->getJss(),
+            'tabs' => $this->getTabs(),
+            'request_uri' => $this->requestStack->getCurrentRequest()->getRequestUri()
         ]);
     }
 
@@ -47,5 +26,43 @@ class BaseController extends AbstractController
         foreach ($items as $key => $value) {
             $this->render_data[$key] = $value;
         }
+    }
+
+    private function getStyles(): array
+    {
+        return [
+            [
+                'href' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+                'integrity' => "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
+                'crossorigin' => "anonymous"
+            ]
+        ];
+    }
+
+    private function getJss(): array
+    {
+        return [
+            [
+                'src' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+                'integrity' => "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM",
+                'crossorigin' => "anonymous"
+            ]
+        ];
+    }
+
+    private function getTabs(): array
+    {
+        return [
+            [
+                'href' => '/',
+                'innerHTML' => 'Home',
+                'content' => 'Home content'
+            ],
+            [
+                'href' => '/enit',
+                'innerHTML' => 'EnIT',
+                'content' => 'EnIT Content'
+            ]
+        ];
     }
 }
--- src/Controller/EnitController.php
+++ src/Controller/EnitController.php
@@ -6,16 +6,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Controller\BaseController;
 
-class MainController extends BaseController
+class EnitController extends BaseController
 {
-    #[Route('/', name: 'main_page')]
+    #[Route('/enit', name: 'enit')]
     public function index(): Response
     {
         $this->addItemsToRender([
             'title' => 'EnIT',
-            'header' => 'Martynovich'
+            'header' => 'EnIT'
         ]);
 
-        return $this->render('main/index.html.twig', $this->render_data);
+        return $this->render('enit/index.html.twig', $this->render_data);
     }
 }
