b181814d6b36bb0e730c5301febe2c5913ef3573
DzianCPP <dzian.cpp@gmail.com>
2023-10-05 20:25:53 +0300

    saving layout

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -35,6 +35,11 @@ abstract class BaseController extends AbstractController
                 'href' => "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
                 'integrity' => "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
                 'crossorigin' => "anonymous"
+            ],
+            [
+                'href' => 'assets/css/global.css',
+                'integrity' => '',
+                'crossorigin' => 'anonymous'
             ]
         ];
     }
