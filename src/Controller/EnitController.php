91103127fbdd1efdde1e70d79e9ec407b785be96
DzianCPP <dzian.cpp@gmail.com>
2023-10-06 09:42:54 +0300

    carousel

--- src/Controller/EnitController.php
+++ src/Controller/EnitController.php
@@ -11,11 +11,35 @@ class EnitController extends BaseController
     #[Route('/enit', name: 'enit')]
     public function index(): Response
     {
-        $this->addItemsToRender([
-            'title' => 'EnIT',
-            'header' => 'EnIT'
-        ]);
+        $this->createItemsToRender();
 
         return $this->render('enit/index.html.twig', $this->render_data);
+    }
+
+    private function createItemsToRender(): void
+    {
+        $this->addItemsToRender(
+            [
+                'title' => 'EnIT',
+                'header' => 'EnIT',
+                'images' => $this->getEnitCarouselImages()
+            ]
+        );
+    }
+
+    private function getEnitCarouselImages(): array
+    {
+        return [
+            [
+                'src' => 'assets/images/enit/carousel_1.png',
+                'alt' => '...',
+                'active' => true
+            ],
+            [
+                'src' => 'assets/images/enit/carousel_2.png',
+                'alt' => '...',
+                'active' => false
+            ]
+        ];
     }
 }
